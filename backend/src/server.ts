f2f31f1918d6a4d15a3ee656e80c6be47ed60999
Jason Thuma <jasonthuma@gmail.com>
2022-10-13 14:19:06 -0400

    modified cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,14 +9,20 @@ import { errorHandler } from "./middleware/errorHandler";
 import userRoutes from "./routes/userRoutes";
 import mountainRoutes from "./routes/mountainRoutes";
 import hikeRoutes from "./routes/hikeRoutes";
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8000",
+      "http://fortysixer2-env.eba-xitaunry.us-east-1.elasticbeanstalk.com",
+    ],
+  })
+);
+
+import { config } from "dotenv";
+config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "http://localhost:8080",
-  })
-);
 
 app.use("/users", userRoutes);
 app.use("/mountains", mountainRoutes);
@@ -24,18 +30,18 @@ app.use("/hikes", hikeRoutes);
 
 app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-app.get("*", (req: Request, res: Response) => {
+app.get("*", (req: Request, res: Response) =>
   res.sendFile(
     path.resolve(__dirname, "../../", "frontend", "build", "index.html")
-  );
-});
+  )
+);
 
 app.use(errorHandler);
 
 AppDataSource.initialize()
   .then(async () => {
-    app.listen(8080, () => {
-      console.log("Server listening on port 8080");
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server listening on port 8000");
     });
   })
   .catch((error) => console.log(error));
